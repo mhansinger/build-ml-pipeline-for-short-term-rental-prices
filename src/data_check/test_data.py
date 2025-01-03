92728888b9bdc29e7932a6bc991594606990f5a4
Maximilian Hansinger <M92520@eon.com>
2025-01-03 13:20:43 +0000

    data_check

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,11 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    """ check if data contains correct number of rows """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """ check if price column values are between min and max value """
+    assert data['price'].between(min_price, max_price).all()
